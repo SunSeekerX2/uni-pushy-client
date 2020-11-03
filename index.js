2b9bdd13389921aa89b1d7e7d19bb3e4bcebc92a
SunSeekerX <1647800606@qq.com>
2020-11-03 17:35:21 +0800

    feat: 0.0.1

--- index.js
+++ index.js
@@ -1,49 +1,63 @@
-/**
- * @name:
- * @author: SunSeekerX
- * @Date: 2020-08-15 09:57:32
- * @LastEditors: SunSeekerX
- * @LastEditTime: 2020-10-14 11:14:33
- */
-
 // 主颜色
-const $mainColor = 'FF5B78'
+let $mainColor = 'FF5B78'
 // 弹窗图标url
-const $iconUrl = './images/ic_ar.png'
+// const $iconUrl = '/static/images/update/ic_ar.png'
+let $iconUrl = ''
 
-class Pushy {
-  // 当前应用的APPID
-  appid = ''
-  // 原生版本名:string
-  nativeVersion = ''
-  //  原生版本号:number
-  nativeVersionCode = 0
-  // wgt 资源版本
-  wgtVersion = ''
-  // wgt 资源版本号
-  wgtVersionCode = 0
-  // 平台
-  platform = ''
-
-  // 个人配置
-  _config = {
-    // 项目id
-    projectId: '',
-    // 是否打开检查更新
-    update: true,
-    // 检查更新地址
-    updateUrl: '',
-    // 是否显示log
-    log: true,
-  }
-
-  // 生效的配置对象
-  _workSetting = {}
-
-  // 版本
-  version = '0.0.1'
-
+export default class Pushy {
   constructor(options = {}) {
+    /**
+     * @name 发布订阅
+     */
+    // 缓存列表
+    this.list = {}
+
+    // 当前应用的APPID
+    this.appid = ''
+    // 原生版本名:string
+    this.nativeVersion = ''
+    //  原生版本号:number
+    this.nativeVersionCode = 0
+    // wgt 资源版本
+    this.wgtVersion = ''
+    // wgt 资源版本号
+    this.wgtVersionCode = 0
+    // 是否初始化完成
+    this._isInitFinish = false
+    // 系统信息
+    this.systemInfo = {}
+    // state
+    this.state = {
+      // 是否正在检查更新
+      isGettingUpdate: false,
+      // 是否正在静默更新
+      isSlientUpdating: false,
+      // 是否已经静默更新
+      isSlientUpdated: false,
+    }
+
+    // 个人配置
+    this._config = {
+      // 项目id
+      projectId: '',
+      // 是否打开检查更新
+      update: true,
+      // 检查更新地址
+      updateUrl: '',
+      // 是否显示log
+      log: true,
+      // 开始检查更新提示
+      startGetUpdateTips: '开始检查更新...',
+      // 正在检查更新提示
+      gettingUpdateTips: '正在检查更新中...',
+    }
+
+    // 初始化计时器
+    // this._initTimer = null
+
+    // 生效的配置
+    this._workSetting = {}
+
     // 合并用户设置
     this.setConfig(options)
 
@@ -52,12 +66,88 @@ class Pushy {
   }
 
   /**
-   * @name 初始化设置方法
+   * @name 发布订阅
+   */
+  // 订阅
+  on(event, fn) {
+    // 如果对象中没有对应的 event 值，也就是说明没有订阅过，就给 event 创建个缓存列表
+    // 如有对象中有相应的 event 值，把 fn 添加到对应 event 的缓存列表里
+    ;(this.list[event] || (this.list[event] = [])).push(fn)
+    return this
+  }
+  // 监听一次
+  once(event, fn) {
+    // 先绑定，调用后删除
+    const _this = this
+    function on() {
+      _this.off(event, on)
+      fn.apply(_this, arguments)
+    }
+    on.fn = fn
+    _this.on(event, on)
+    return _this
+  }
+  // 取消订阅
+  off(event, fn) {
+    const _this = this
+    const fns = _this.list[event]
+    // 如果缓存列表中没有相应的 fn，返回false
+    if (!fns) return false
+    if (!fn) {
+      // 如果没有传 fn 的话，就会将 event 值对应缓存列表中的 fn 都清空
+      fns && (fns.length = 0)
+    } else {
+      // 若有 fn，遍历缓存列表，看看传入的 fn 与哪个函数相同，如果相同就直接从缓存列表中删掉即可
+      let cb
+      for (let i = 0, cbLen = fns.length; i < cbLen; i++) {
+        cb = fns[i]
+        if (cb === fn || cb.fn === fn) {
+          fns.splice(i, 1)
+          break
+        }
+      }
+    }
+    return _this
+  }
+  // 发布
+  emit() {
+    const _this = this
+    // 第一个参数是对应的 event 值，直接用数组的 shift 方法取出
+    const event = [].shift.call(arguments)
+    // 如果缓存列表里没有 fn 就返回 false
+    if (!_this.list[event]) {
+      return false
+    }
+
+    // const fns = [..._this.list[event]]
+    // // 如果缓存列表里没有 fn 就返回 false
+    // if (!fns || fns.length === 0) {
+    //   return false
+    // }
+    // 遍历 event 值对应的缓存列表，依次执行 fn
+    _this.list[event].forEach(fn => {
+      fn.apply(_this, arguments)
+    })
+    return _this
+  }
+
+  /**
+   * @name 初始化设置方法，该方法内无法发布事件
    * @param { Object } options
+   * @return { Void }
    */
   setConfig(options) {
     // 公开可重写的设置key
-    const _publicSettings = ['projectId', 'update', 'updateUrl', 'log']
+    const _publicSettings = [
+      'projectId',
+      'update',
+      'updateUrl',
+      'log',
+      'startGetUpdateTips',
+      'gettingUpdateTips',
+      'mainColor',
+			'logo'
+    ]
     // 合并传入的设置对象，如果传入了非公开设置的key会被丢弃
     const _workSetting = {}
     for (const item of Object.keys(options)) {
@@ -65,6 +155,12 @@ class Pushy {
         _workSetting[item] = options[item]
       }
     }
+    if (_workSetting.mainColor) {
+      $mainColor = _workSetting.mainColor
+    }
+		if(_workSetting.logo){
+			$iconUrl = _workSetting.logo
+		}
     // 合并设置对象
     Object.assign(this._config, _workSetting)
     // 给出生效设置对象
@@ -73,62 +169,221 @@ class Pushy {
 
   /**
    * @name 初始化请求参数
+   * @return { Promise<Boolean> }
    */
   init() {
-    // 获取原生版本参数
-    const { appid, version, versionCode } = plus.runtime
+    return new Promise(resolve => {
+      // 获取原生版本参数
+      const { appid, version, versionCode } = plus.runtime
+      // appid
+      this.appid = appid
+      // 原生版本
+      this.nativeVersion = version
+      // 原生版本号
+      this.nativeVersionCode = Number(versionCode)
+      // 系统信息
+      this.systemInfo = uni.getSystemInfoSync()
 
-    this.appid = appid
-    this.nativeVersion = version
-    this.nativeVersionCode = Number(versionCode)
-    this.platform = uni.getSystemInfoSync().platform
-
-    // 获取wgt资源版本
-    plus.runtime.getProperty(appid, res => {
-      const { version, versionCode } = res
-      this.wgtVersion = version
-
-      /**
-       * @description 这里 uni-app 有个 bug 在 ios versionCode 始终返回 ipa 的 versionCode ，无法获取 wgt 的版本号，可以获取版本名。
-       * 上传资源版本名贺版本号一定要对应，版本名 x.y.z:string, 版本号 xyz:number
-       */
-      if (this.platform === 'android') {
+      // 读取 wgt 版本号
+      plus.runtime.getProperty(appid, res => {
+        const { version, versionCode } = res
+        this.wgtVersion = version
         this.wgtVersionCode = Number(versionCode)
-      } else {
-        this.wgtVersionCode = Number(version.split('.').join(''))
-      }
-
-      // 检查更新
-      this.onUpdate()
+        /**
+         * @description 这里 uni-app 有个 bug 在 ios versionCode 始终返回 ipa 的 versionCode ，无法获取 wgt 的版本号，可以获取版本名。
+         * 上传资源版本名贺版本号一定要对应，版本名 x.y.z:string, 版本号 xyz:number
+         * uni-app 更新日志已经修复，等待测试
+         */
+        // if (this.systemInfo.platform === 'android') {
+        //   this.wgtVersionCode = Number(versionCode)
+        // } else {
+        //   this.wgtVersionCode = Number(version.split('.').join(''))
+        // }
+        // 获取设备信息
+        plus.device.getInfo({
+          success: deviceInfo => {
+            this.systemInfo.uuid = deviceInfo.uuid
+          },
+          fail: e => {
+            console.error(e)
+          },
+          complete: () => {
+            // 初始化成功
+            this._isInitFinish = true
+            // 发布 onInit 事件
+            this.emit('onInitFinish')
+            // 成功返回
+            resolve(true)
+          },
+        })
+      })
     })
   }
 
   /**
-   * @name 请求服务端版本资源参数
+   * 获取资源信息
    */
-  async onUpdate() {
-    const { platform } = this
-    const { update, updateUrl } = this._config
-
-    if (!update) {
-      return '更新被关闭'
-    } else if (!updateUrl) {
-      return '无检查更新地址'
+  async getInfo() {
+    // 判断是否初始化
+    if (!this._isInitFinish) {
+      // 没有初始化进行初始化
+      await this.init()
     }
 
-    try {
-      const res = await this.onRequestUpdate()
-      if (!res) {
-        return '暂无更新'
-      }
-      const { url, isForceUpdate } = res
-      if (isForceUpdate) {
-        if (/\.wgt$/i.test(url) || platform === 'android') {
-          getDownload(res)
-        } else {
-          plus.runtime.openURL(url)
-        }
-      } else {
+    return Promise.resolve({
+      // 当前应用的APPID
+      appid: this.appid,
+      // 原生版本名:string
+      nativeVersion: this.nativeVersion,
+      //  原生版本号:number
+      nativeVersionCode: this.nativeVersionCode,
+      // wgt 资源版本
+      wgtVersion: this.wgtVersion,
+      // wgt 资源版本号
+      wgtVersionCode: this.wgtVersionCode,
+      // 系统信息
+      systemInfo: this.systemInfo,
+      // 个人配置
+      _config: this._config,
+      // 生效的配置
+      _workSetting: this._workSetting,
+    })
+  }
+
+  /**
+   * @name 检查更新，只会 resolve， 不会 reject
+   * @return { Promise<object> } 包装的结果对象，
+   * statusCode：
+   * 251：需要更新原生版本
+   *    data native包信息
+   *    response 原生响应对象
+   *    info pushy.getInfo() 返回
+   * 252：需要更新wgt版本
+   *    data wgt包信息
+   *    response 原生响应对象
+   *    info pushy.getInfo() 返回
+   * 253：暂无更新
+   *    response 原生响应对象
+   *    info pushy.getInfo() 返回
+   * 254：请求成功，但返回失败
+   *    response 原生响应对象
+   *    info pushy.getInfo() 返回
+   * 451: 更新被关闭，用户手动配置关闭了 message
+   * 452：用户未配置更新地址 message
+   * 453：正在检查更新 message
+   * 454：正在静默更新 message
+   * 455：已经静默更新完成需要重启 message
+   * 500：请求失败，查看返回对象 message 获取错误详情描述 error：原生错误对象
+   * 505：占位，应该不会返回这个
+   */
+  async getUpdate() {
+    // 判断是否正在更新
+    if (this.state.isGettingUpdate) {
+      return Promise.resolve({
+        statusCode: 453,
+        message: '正在更新中',
+      })
+    }
+    // 判断是否正在静默更新
+    if (this.state.isSlientUpdating) {
+      return Promise.resolve({
+        statusCode: 454,
+        message: '正在静默更新',
+      })
+    }
+    // 判断是否已经静默更新
+    if (this.state.isSlientUpdated) {
+      return Promise.resolve({
+        statusCode: 455,
+        message: '已经静默更新完成，需要重启App生效',
+      })
+    }
+    // 打开正在更新
+    this.state.isGettingUpdate = true
+
+    // 判断是否初始化
+    if (!this._isInitFinish) {
+      // 没有初始化进行初始化
+      await this.init()
+    }
+    // 发布 onStartGetUpdate - 开始检查更新
+    this.emit('onStartGetUpdate')
+
+    const { update, updateUrl } = this._config
+    // 条件判断
+    if (!update) {
+      return Promise.resolve({
+        statusCode: 451,
+        message: '更新被关闭',
+      })
+    } else if (!updateUrl) {
+      return Promise.resolve({
+        statusCode: 452,
+        message: '无检查更新地址',
+      })
+    }
+
+    // 提示检查更新
+    toast(this._config.gettingUpdateTips)
+    // 网络请求
+    const res = await this.onRequestUpdate()
+    // 关闭正在更新
+    this.state.isGettingUpdate = false
+    // 根据 statusCode 处理结果
+    switch (res.statusCode) {
+      case 251:
+        // 251：需要更新原生版本
+        // 发布需要更新原生版本事件
+        this.emit('onNativeUpdateRequired')
+        this.startUpdate(res.data)
+        return Promise.resolve(res)
+      case 252:
+        // 252：需要更新wgt版本
+        // 发布需要更新wgt版本事件
+        this.emit('onWgtUpdateRequired')
+        this.startUpdate(res.data)
+        return Promise.resolve(res)
+      case 253:
+        // 253：暂无更新
+        // 发布 onNoUpdate - 暂无更新
+        this.emit('onNoUpdate')
+        this._config.log &&
+          console.log('Uni-pushy：暂无新版本，接口响应 >>>', res.response)
+        this._config.log &&
+          console.log('Uni-pushy：暂无新版本，本地版本 >>>', res.info)
+        // resolve(false)
+        return Promise.resolve(res)
+      case 254:
+        // 254：请求成功，但返回失败
+        this._config.log &&
+          console.log('Uni - pushy：success: false >>>>>>>>>', res.response)
+        return Promise.resolve(res)
+      case 500:
+        // 500：请求失败，查看返回对象 message 获取错误详情描述 error：原生错误对象
+        this._config.log &&
+          console.log('Uni - pushy：500: false >>>>>>>>>', res.error)
+        return Promise.resolve(res)
+      default:
+        return Promise.resolve({
+          statusCode: 505,
+          message: '应该不会返回这个',
+        })
+    }
+  }
+
+  /**
+   * @name 开始更新，有原生版本和wgt版本更新
+   */
+  async startUpdate(res) {
+    const { platform } = this.systemInfo
+    /**
+     * updateType 更新类型（1：用户同意更新，2：强制更新，3：静默更新）
+     */
+    const { url, updateType } = res
+
+    switch (updateType) {
+      case 1:
+        // 用户同意更新
         updatePopup(res, () => {
           if (/\.wgt$/i.test(url) || platform === 'android') {
             getDownload(res)
@@ -136,76 +391,213 @@ class Pushy {
             plus.runtime.openURL(url)
           }
         })
-      }
-    } catch (e) {
-      return `检查更新出错，错误详情：${e.message}`
+        break
+
+      case 2:
+        // 强制更新
+        if (/\.wgt$/i.test(url) || platform === 'android') {
+          getDownload(res)
+        } else {
+          plus.runtime.openURL(url)
+        }
+        break
+
+      case 3:
+        // 静默更新
+        this.state.isSlientUpdating = true
+        this.handleUpdateSilent(res)
+        break
+
+      default:
+        break
     }
   }
 
   /**
-   * @name 请求服务器版本号
+   * @name 请求服务器版本号，只做网络请求，返回数据，不做任何逻辑处理,已经封装 Promise 不会触发 reject
+   *
+   * statusCode：
+   * 251：需要更新原生版本
+   *    message：string 详情描述
+   *    response 原生响应对象
+   *    data native包
+   * 252：需要更新wgt版本
+   *    message：string 详情描述
+   *    response 原生响应对象
+   *    data wgt包
+   * 253：暂无更新
+   *    message：string 详情描述
+   *    response 原生响应对象
+   * 254：请求成功，但返回失败
+   *    message：string 详情描述
+   *    response 原生响应对象
+   * 500：请求失败
+   *    message：string 详情描述
+   *    error：原生错误对象
+   * @return { Promise<object> } 包装的响应对象
    */
   async onRequestUpdate() {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
+      const { platform } = this.systemInfo
       const { updateUrl, projectId } = this._config
 
-      const { wgtVersionCode, nativeVersionCode, platform } = this
+      const {
+        wgtVersion,
+        wgtVersionCode,
+        nativeVersion,
+        nativeVersionCode,
+        systemInfo,
+      } = this
 
       uni.request({
         url: `${updateUrl}/api/update`,
+        method: 'POST',
         data: {
           projectId,
-          versionCode: wgtVersionCode,
-          nativeVersionCode: nativeVersionCode,
-          platform: platform,
+          wgtVersion,
+          wgtVersionCode,
+          nativeVersion,
+          nativeVersionCode,
+          platform,
+          systemInfo,
         },
         success: res => {
           const data = res.data
-          // this._config.log && (console.log('Uni - pushy>>>>>>>>>', data, this))
-
           if (data.success) {
             const { wgt, native } = data.data
             if (native && native.versionCode > nativeVersionCode) {
-              resolve(native)
+              resolve({
+                statusCode: 251,
+                data: native,
+                message: '需要更新原生版本',
+                response: data,
+              })
             } else if (
               wgt &&
               wgt.nativeVersionCode === nativeVersionCode &&
               wgt.versionCode > wgtVersionCode
             ) {
-              resolve(wgt)
+              resolve({
+                statusCode: 252,
+                data: wgt,
+                message: '需要更新wgt版本',
+                response: data,
+              })
             } else {
-              this._config.log &&
-                console.log('Uni pushy：暂无新版本，接口响应 >>>', data)
-              this._config.log &&
-                console.log('Uni pushy：暂无新版本，本地版本 >>>', this)
-              resolve(false)
+              resolve({
+                statusCode: 253,
+                message: '暂无更新',
+                response: data,
+              })
             }
           } else {
-            this._config.log &&
-              console.log('Uni - pushy：success: false >>>>>>>>>', data)
-            resolve(false)
+            resolve({
+              statusCode: 254,
+              message: data.message,
+              response: data,
+            })
           }
         },
         fail: e => {
-          console.log('请求失败', e.message)
-          reject(e)
+          resolve({
+            statusCode: 500,
+            message: e.message,
+            error: e,
+          })
         },
       })
+    })
+  }
+
+  /**
+   * @name 静默更新
+   */
+  async handleUpdateSilent(res) {
+    const { url } = res
+
+    // 创建下载对象
+    const dtask = plus.downloader.createDownload(
+      url,
+      {
+        // 保存文件路径仅支持以"_downloads/"、"_doc/"、"_documents/"开头的字符串。 文件路径以文件后缀名结尾（如"_doc/download/a.doc"）表明指定保存文件目录及名称，以“/”结尾则认为指定保存文件的目录（此时程序自动生成文件名）。 如果指定的文件已经存在，则自动在文件名后面加"(i)"，其中i为数字，如果文件名称后面已经是此格式，则数字i递增，如"download(1).doc"。 默认保存目录为（"_downloads"），并自动生成文件名称。
+        filename: '_doc/update/',
+        // 数值类型，单位为s(秒)，默认值为120s。 超时时间为服务器响应请求的时间（不是下载任务完成的总时间），如果设置为0则表示永远不超时。
+        timeout: 60,
+        // 数值类型，默认为重试3次。
+        retry: 3,
+        // 下载任务重试间隔时间 数值类型，单位为s(秒)，默认值为30s。
+        retryInterval: 30,
+      },
+      // 当下载任务下载完成时触发，成功或失败都会触发。
+      (download, status) => {
+        if (status == 200) {
+          plus.runtime.install(
+            download.filename,
+            {
+              // 是否强制安装
+              force: false,
+            },
+            () => {
+              this.state.isSlientUpdating = false
+              this.state.isSlientUpdated = true
+              this._config.log && console.log('Uni-pushy：静默更新完成 >>>')
+            },
+            e => {
+              this.state.isSlientUpdating = false
+              plus.nativeUI.alert('安装文件失败[' + e.code + ']：' + e.message)
+            },
+          )
+        } else {
+          this.state.isSlientUpdating = false
+          // toast('静默更新文件下载失败...')
+        }
+      },
+    )
+
+    // 启动下载对象
+    dtask.start()
+
+    // 添加下载监听
+    dtask.addEventListener('statechanged', (download, status) => {
+      switch (download.state) {
+        case 1:
+          // 开始
+          this._config.log && console.log('Uni-pushy：下载任务开始请求')
+          break
+        case 2:
+          // 已连接到服务器
+          this._config.log &&
+            console.log(
+              'Uni-pushy：下载任务网络连接已建立，服务器返回响应，准备传输数据内容。',
+            )
+          break
+        case 3:
+          // 下载中...
+          // const progress = parseInt(
+          //   (download.downloadedSize / download.totalSize) * 100,
+          // )
+          // console.log(`Uni-pushy：下载中...：${progress}%`)
+          break
+        case 4:
+          // 下载任务已完成
+          this._config.log && console.log(`Uni-pushy：下载任务已完成`)
+          break
+      }
     })
   }
 }
 
 // 从服务器下载应用资源包（wgt文件）
 const getDownload = function (res) {
-  const { url, isForceUpdate } = res
+  const { url } = res
 
   let popupData = {
     progress: true,
     buttonNum: 2,
   }
-  if (isForceUpdate) {
-    popupData.buttonNum = 0
-  }
+  // if (isForceUpdate) {
+  //   popupData.buttonNum = 0
+  // }
   let dtask
   let lastProgressValue = 0
   let popupObj = downloadPopup(popupData)
@@ -406,9 +798,10 @@ function updatePopup(res, callback) {
   const viewContentWidth = parseInt(popupViewWidth - viewContentPadding * 2)
   // 描述的列表
   const descriptionList = drawtext(changelog || '', viewContentWidth)
+  const imgHeight = $iconUrl ? 20 : 0
   // 弹窗容器高度
-  let popupViewHeight = 80 + 20 + 20 + 90 + 10
-  console.log({popupViewWidth});
+  let popupViewHeight = 80 + 20 + 90 + 10 + imgHeight
+  // let popupViewHeight = 80 + 20 + 20 + 90 + 10
   let popupViewContentList = [
     {
       src: $iconUrl,
@@ -432,7 +825,7 @@ function updatePopup(res, callback) {
         whiteSpace: 'normal',
       },
       position: {
-        top: '90px',
+        top: `${70 + imgHeight}px`,
         left: viewContentPadding + 'px',
         width: viewContentWidth + 'px',
         height: '30px',
@@ -440,7 +833,7 @@ function updatePopup(res, callback) {
     },
   ]
   const textHeight = 18
-  let contentTop = 130
+  let contentTop = 110 + imgHeight
 
   descriptionList.forEach((item, index) => {
     if (index > 0) {
@@ -965,4 +1358,19 @@ function downloadPopup(data) {
   return callbackData
 }
 
-export default Pushy
+/**
+ * @param { String } msg 需要显示的消息
+ * @param { Object } options 配置,同uni-app官网
+ */
+function toast(msg, options) {
+  uni.showToast(
+    Object.assign(
+      {
+        icon: 'none',
+        title: msg,
+        duration: 2000,
+      },
+      options,
+    ),
+  )
+}
